a56434565ee0ddda3eb2f25d2641f9fe67125a3b
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-09-04 18:53:59 +0530

    Update Practice Question 7 .py

--- Practice Question 7 .py
+++
@@ -1,9 +0,0 @@
-d = int(input("Please enter the Date "))
-m = int(input("Please enter the month "))
-y = int(input("Please enter the year "))
-
-if ( 0==d or d>31):
-    print("The data you have entered is invalid")
-if (0==m or m>12):
-    print("The data you have entered is invalid")
-if ()
