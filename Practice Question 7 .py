ee7b7e1977c3e13c4f8a3df238a7790ef959610e
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-09-04 19:10:06 +0530

    Update Practice Question 7 .py

---
+++ Practice Question 7 .py
@@ -0,0 +1,12 @@
+from curses import delay_output
+from time import sleep
+
+
+import math
+import time
+print("Hello please enter the dtae in terms of date, month and year ")
+time.sleep(1)
+a = eval(input("Please enter the Date "))
+b = eval(input("Please enter the Month "))
+c = eval(input("Please enter the Year "))
+
