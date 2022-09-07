3fc7d29710df7745512f2734ac17b165cca75aa5
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-09-07 22:02:13 +0530

    Update Practice Question Looping 1.py

--- Practice Question Looping 1.py
+++ Practice Question Looping 1.py
@@ -1,9 +1,9 @@
 import math
+import string
 import time
 a = eval(input("Please enter the number in order to find its factorial "))
 from math import factorial
 
-for i in range (1 , a + 1):
-    b = factorial*i
-    print(factorial)
+for i in range (1 , a+1 ):
+    print(i)
     
