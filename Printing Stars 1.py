f84bbfb82f6b2f63ded5ca93b87ba64b98b4d2c9
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-09-08 08:25:25 +0530

    Stars Printing Questions

---
+++ Printing Stars 1.py
@@ -0,0 +1,6 @@
+st = 1
+for i in range (1,6):
+    for k in range (1, st + 1):
+        print("*", end =" ")
+    st = st+1
+    print()
---
+++ Printing Stars.py
@@ -0,0 +1,10 @@
+st = 1
+sp = 4
+for i in range (1,6):
+    for j in range(1, sp+1):
+        print(" ", end =" ") 
+    for k in range (1, st + 1):
+        print("*", end =" ")
+    sp = sp-1
+    st = st+1
+    print()
