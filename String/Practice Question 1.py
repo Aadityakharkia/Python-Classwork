e15f05fa6c5d9f30d23e14fd24c316223b22eb83
Aaditya Kharkia <aadityakharkia10@gmail.com>
2022-10-10 08:16:31 +0530

    Updated

--- String/Practice Question 1.py
+++ String/Practice Question 1.py
@@ -1,9 +1,8 @@
-a= input("Please enter a sentence H")
-b = 0 
-for i in a(0,10):
-    if (a==a):
-        b=b+1
-    else:
-        print()
-
-print(b)
+s= input("Please enter a sentence ")
+l = len(s)
+c = 0 
+for i in range(0,l):
+    match(s[i]):
+         case 'A'| 'E'| 'I'| 'O'| 'U'| 'a'| 'e'| 'i'| 'o'| 'u':
+            c=c+1
+print(" Number of letter a in the sentence ",s, "is",c)
